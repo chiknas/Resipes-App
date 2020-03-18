8c1447324ec48fe96d5e0c76b6a065b80e6bc9b7
chiknas <nkuknas@gmail.com>
2020-03-18 12:56:56 +0000

    mock mvc test

--- src/test/java/com/chiknas/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/chiknas/recipe/controllers/IndexControllerTest.java
@@ -1,17 +1,26 @@
 package com.chiknas.recipe.controllers;
 
+import com.chiknas.recipe.model.Recipe;
 import com.chiknas.recipe.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anySet;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * Index controller unit tests.
@@ -35,12 +44,26 @@ class IndexControllerTest {
     }
 
     @Test
+    void testMVC() throws Exception {
+        MockMvc mvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, "/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
     void getIndex() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+        ArgumentCaptor<Set<Recipe>> recipeCaptor = ArgumentCaptor.forClass(Set.class);
+
         String index = indexController.getIndex(model);
 
         assertEquals("index", index);
-
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(any(), anySet());
+        verify(model, times(1)).addAttribute(any(), recipeCaptor.capture());
+        assertEquals(recipeCaptor.getValue().size(), 1);
     }
 }
